b57db951abffc4ff42c40eee43b57d1e23e705ac
rayhanbabu <rayhanbabu458@gmail.com>
2023-08-15 11:45:21 +0600

    index.js change

--- app.js
+++ app.js
@@ -13,34 +13,30 @@ const cors=require('cors');
 const mongoose=require('mongoose');
 
 
- //Security Middleware Impliment
- app.use(cors());
- app.use(helmet());
- app.use(mongoSanitize());
- app.use(xss());
- app.use(hpp());
+  //Security Middleware Impliment
+  app.use(cors());
+  app.use(helmet());
+  app.use(mongoSanitize());
+  app.use(xss());
+  app.use(hpp());
 
- //body perser
- app.use(bodyParser.json())
-
-
- //Rate Limite
- const Limiter=rateLimit({windowMs:15*60*100,max:3000});
+  //body perser
+  app.use(bodyParser.json())
 
 
-  //Database
-  mongoose.connect("mongodb+srv://rayhanbabu458:rayhanbabu458@atlascluster.q16wgcx.mongodb.net/blogs")
+    //Rate Limite
+    const Limiter=rateLimit({windowMs:15*60*100,max:3000});
+
+    //Database
+    mongoose.connect("mongodb+srv://rayhanbabu458:rayhanbabu458@atlascluster.q16wgcx.mongodb.net/blogs")
        .then(()=>{
-             //console.log("run");
+              //console.log("run");
          })
        .catch((err) => console.log(err));
+             //Managing BackEnd API Routing  localhost:6000/api/v1
+   app.use('/api/v1',router)
 
-
-
-   //Managing BackEnd API Routing  localhost:6000/api/v1
-  app.use('/api/v1',router)
-
-module.exports=app;
+   module.exports=app;
 
 
 
